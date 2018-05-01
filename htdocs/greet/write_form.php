c46e19bc55d411c7cbd79dd85925d96e7fd2a58d
jun82 <20164127@gwangju.ac.kr>
2018-05-01 16:01:09 +0900

    adsf

--- htdocs/greet/write_form.php
+++ htdocs/greet/write_form.php
@@ -1,6 +1,6 @@
 <?
 	session_start();
-	$html_ok = $_POST['html_ok'];
+	// $html_ok = $_POST['html_ok'];
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
